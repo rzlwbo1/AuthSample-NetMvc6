dbd284c8153e9e391141341f4930cace16b0d0b6
rzlwbo1 <rizalwb2@gmail.com>
2023-11-02 22:03:49 +0700

    adding controllers admin,dashboard,auth and views

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddDefaultTokenProviders();
 
 builder.Services.ConfigureApplicationCookie(opt => 
-    opt.LoginPath = "/UserAuth/Login"
+    opt.LoginPath = "/Authentication/Login"
     );
 
 builder.Services.AddScoped<IUserAuthService, UserAuthService>();
@@ -47,6 +47,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Authentication}/{action=Login}/{id?}");
 
 app.Run();
